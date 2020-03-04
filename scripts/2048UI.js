371c588a93dc2686d11c30a4db7fa64cbea1d67c
Yufan Wang <yufanwang3@gmail.com>
2020-03-04 00:52:49 -0500

    remove junk file & create struct for game || UI

---
+++ scripts/2048UI.js
@@ -0,0 +1,26 @@
+var a2048UI=function(){
+    var self = this;
+    this.game = undefined;
+    this.running = false;
+
+    this.initialize = function(){
+        // TODO:: implement initialization function
+
+        // maybe move key detection here
+
+        // definitelly btn detection
+    };
+
+    this.refreshView = function(){
+
+    };
+
+    this.takeMove = function(){
+
+    };
+
+    this.updateUI = function(){
+        
+    }
+
+}
